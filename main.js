5602f12523a95abbc8e87b1ababf293140fcd35a
ayana-ismagambetova <ayana.ismagambetova@nu.edu.kz>
2022-12-04 15:01:27 +0600

    final project files added

---
+++ main.js
@@ -0,0 +1,19 @@
+function menuExpand() {
+  document.getElementById("menubars").classList.toggle("change");
+  document.getElementById("navbar").classList.toggle("change");
+  document.getElementById("menunew").classList.toggle("changemenu");
+}
+
+  function Hide()
+{
+  var control = document.getElementById('courses');
+ control.style.display = "none";
+}
+
+function Show()
+{
+ var control = document.getElementById('courses');
+ control.style.display = "block";
+
+}
+
